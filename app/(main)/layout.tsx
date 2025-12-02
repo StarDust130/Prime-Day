5d7f8c3bbac2dffd286afaef23a372b76a5d4d81
StarDust130 <csyadav0513@gmail.com>
2025-12-02 21:33:58 +0530

    feat: create RootLayout component with Header integration

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/ui/Header";
+
+
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="flex-1 ">{children}</main>
+        </div>
+    );
+}
