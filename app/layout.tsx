501609c11a5fd7b79351f86a87954ed632953196
StarDust130 <csyadav0513@gmail.com>
2025-12-03 02:13:46 +0530

    refactor: remove unused Clerk components and simplify RootLayout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
 import { ThemeProvider } from "@/lib/theme-provider";
-import { ModeToggle } from "@/components/ui/ModeToggle";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,35 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+  
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased  `}
         >
-          {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-            <ModeToggle />
-          </header> */}
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
+   
             {children}
-          </ThemeProvider>
+        
         </body>
       </html>
-    </ClerkProvider>
+
   );
 }
