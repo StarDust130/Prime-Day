d81bf50eaf0f44c59b8f3fec67523834baf3f2c7
StarDust130 <csyadav0513@gmail.com>
2025-12-02 22:17:42 +0530

    feat: add BottomNav component to RootLayout for improved navigation

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import BottomNav from "@/components/ui/BottomNav";
 import Header from "@/components/ui/Header";
 
 
@@ -11,6 +12,7 @@ export default function RootLayout({
         <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1 ">{children}</main>
+            <BottomNav />
         </div>
     );
 }
